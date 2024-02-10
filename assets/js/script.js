23c9ac5f8e3566d2d3ac0426910727e8b4e0ad7d
talhajutt-01 <talhaj928@gmail.com>
2024-02-10 16:29:54 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,408 @@
+const title = document.getElementById('title');
+
+const nav0 = document.getElementById('nav0');
+
+
+const h0 = document.getElementById('h0');
+const h1 = document.getElementById('h1');
+const h2 = document.getElementById('h2');
+
+const s0 = document.getElementById('s0');
+const s1 = document.getElementById('s1');
+const s2 = document.getElementById('s2');
+const s3 = document.getElementById('s3');
+const s4 = document.getElementById('s4');
+const s5 = document.getElementById('s5');
+const s6 = document.getElementById('s6');
+const s7 = document.getElementById('s7');
+const s8 = document.getElementById('s8');
+const s9 = document.getElementById('s9');
+const s10 = document.getElementById('s10');
+const s11 = document.getElementById('s11');
+const s12 = document.getElementById('s12');
+const s13 = document.getElementById('s13');
+const s14 = document.getElementById('s14');
+const s15 = document.getElementById('s15');
+const s16 = document.getElementById('s16');
+const s17 = document.getElementById('s17');
+const s18 = document.getElementById('s18');
+const s19 = document.getElementById('s19');
+const s20 = document.getElementById('s20');
+const s21 = document.getElementById('s21');
+const s22 = document.getElementById('s22');
+const s23 = document.getElementById('s23');
+const s24 = document.getElementById('s24');
+const s25 = document.getElementById('s25');
+const s26 = document.getElementById('s26');
+const s27 = document.getElementById('s27');
+const s28 = document.getElementById('s28');
+const s29 = document.getElementById('s29');
+const s30 = document.getElementById('s30');
+
+const se0 = document.getElementById('se0');
+const se1 = document.getElementById('se1');
+const se2 = document.getElementById('se2');
+const se3 = document.getElementById('se3');
+const se4 = document.getElementById('se4');
+const se5 = document.getElementById('se5');
+
+const n0 = document.getElementById('n0');
+const n1 = document.getElementById('n1');
+const n2 = document.getElementById('n2');
+const n3 = document.getElementById('n3');
+const n4 = document.getElementById('n4');
+const n5 = document.getElementById('n5');
+const n6 = document.getElementById('n6');
+const n7 = document.getElementById('n7');
+const n8 = document.getElementById('n8');
+const n9 = document.getElementById('n9');
+const n10 = document.getElementById('n10');
+const n11 = document.getElementById('n11');
+const n12 = document.getElementById('n12');
+const n13 = document.getElementById('n13');
+
+const fc0 = document.getElementById('fc0');
+const fc1 = document.getElementById('fc1');
+const fc2 = document.getElementById('fc2');
+const fc3 = document.getElementById('fc3');
+const fc4 = document.getElementById('fc4');
+const fc5 = document.getElementById('fc5');
+const fc6 = document.getElementById('fc6');
+const fc7 = document.getElementById('fc7');
+const fc8 = document.getElementById('fc8');
+const fc9 = document.getElementById('fc9');
+const fc10 = document.getElementById('fc10');
+const fc11 = document.getElementById('fc11');
+const fc12 = document.getElementById('fc12');
+const fc13 = document.getElementById('fc13');
+const fc14 = document.getElementById('fc14');
+const fc15 = document.getElementById('fc15');
+const fc16 = document.getElementById('fc16');
+const fc17 = document.getElementById('fc17');
+const fc18 = document.getElementById('fc18');
+const fc19 = document.getElementById('fc19');
+const fc20 = document.getElementById('fc20');
+const fc21 = document.getElementById('fc21');
+const fc22 = document.getElementById('fc22');
+const fc23 = document.getElementById('fc23');
+const fc24 = document.getElementById('fc24');
+const fc25 = document.getElementById('fc25');
+const fc26 = document.getElementById('fc26');
+const fc27 = document.getElementById('fc27');
+const fc28 = document.getElementById('fc28');
+const fc29 = document.getElementById('fc29');
+const fc30 = document.getElementById('fc30');
+const fc31 = document.getElementById('fc31');
+const fc32 = document.getElementById('fc32');
+const fc33 = document.getElementById('fc33');
+const fc34 = document.getElementById('fc34');
+const fc35 = document.getElementById('fc35');
+const fc36 = document.getElementById('fc36');
+const fc37 = document.getElementById('fc37');
+const fc38 = document.getElementById('fc38');
+const fc39 = document.getElementById('fc39');
+const fc40 = document.getElementById('fc40');
+const fc41 = document.getElementById('fc41');
+const fc42 = document.getElementById('fc42');
+const fc43 = document.getElementById('fc43');
+const fc44 = document.getElementById('fc44');
+const fc45 = document.getElementById('fc45');
+const fc46 = document.getElementById('fc46');
+const fc47 = document.getElementById('fc47');
+const fc48 = document.getElementById('fc48');
+const fc49 = document.getElementById('fc49');
+const fc50 = document.getElementById('fc50');
+
+const c0 = document.getElementById('c0');
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
+const c5 = document.getElementById('c5');
+const c6 = document.getElementById('c6');
+const c7 = document.getElementById('c7');
+const c8 = document.getElementById('c8');
+const c9 = document.getElementById('c9');
+
+
+const f0 = document.getElementById('f0');
+const f1 = document.getElementById('f1');
+const f2 = document.getElementById('f2');
+const f3 = document.getElementById('f3');
+const f4 = document.getElementById('f4');
+const f5 = document.getElementById('f5');
+const f6 = document.getElementById('f6');
+const f7 = document.getElementById('f7');
+const f8 = document.getElementById('f8');
+const f9 = document.getElementById('f9');
+
+
+const g0 = document.getElementById('nav');
+const g1 = document.getElementById('header');
+const g2 = document.getElementById('selection');
+const g3 = document.getElementById('service');
+const g4 = document.getElementById('newcars');
+const g5 = document.getElementById('featuredcars');
+const g6 = document.getElementById('clientssay');
+const g7 = document.getElementById('brand');
+const g8 = document.getElementById('blog');
+const g9 = document.getElementById('contact');
+const g10 = document.getElementById('footer');
+
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
+const img6 = document.getElementById('img6');
+const img7 = document.getElementById('img7');
+const img8 = document.getElementById('img8');
+const img9 = document.getElementById('img9');
+const img10 = document.getElementById('img10');
+const img11 = document.getElementById('img11');
+const img12 = document.getElementById('img12');
+const img13 = document.getElementById('img13');
+const img14 = document.getElementById('img14');
+const img15 = document.getElementById('img15');
+const img16 = document.getElementById('img16');
+const img17 = document.getElementById('img17');
+const img18 = document.getElementById('img18');
+const img19 = document.getElementById('img19');
+const img20 = document.getElementById('img20');
+
+
+// Function to update content for a specific element
+function updateContent(element, newValue) {
+    // Update content for the element
+    element.textContent = newValue;
+  }
+  
+  // Function to update content for an <a> tag
+  function updateLinkContent(linkElement, newValue) {
+    // Update content for the <a> tag
+    linkElement.innerText = newValue;
+  }
+
+  function updateImageSrc(imageElement, newSrc) {
+    // Update the src attribute of the image element
+    imageElement.src = newSrc;
+}
+
+function updateBackgroundColor(element, newColor) {
+  // Update the background color for the element
+  element.style.backgroundColor = newColor;
+}
+
+  
+  // Fetch JSON data from the text file
+  function updatePageContent(pageName) {
+    console.log('Updating content for page:', pageName);
+    const fetchJson1 = fetch('t.txt')
+    .then(response => response.json());
+
+// Fetch the second JSON file
+const fetchJson2 = fetch('image.txt')
+    .then(response => response.json());
+
+// Wait for both fetch operations to complete
+Promise.all([fetchJson1, fetchJson2])
+    .then(jsonDataArray => {
+        // Merge the data from both JSON files
+        const mergedData = Object.assign({}, ...jsonDataArray);
+            
+           
+           
+            // Extract the filename from the path
+            const fileName = pageName.split('/').pop();
+
+            if (fileName === 'index.html' || fileName === '') {
+
+                updateContent(title, mergedData.title.title);
+
+                updateLinkContent(nav0, mergedData.nav.nav0);
+                
+                
+                updateContent(h0, mergedData.header.h0);
+                updateContent(h1, mergedData.header.h1);
+                updateContent(h2, mergedData.header.h2);
+
+                updateContent(s0, mergedData.selection.s0);
+                updateContent(s1, mergedData.selection.s1);
+                updateContent(s2, mergedData.selection.s2);
+                updateContent(s3, mergedData.selection.s3);
+                updateContent(s4, mergedData.selection.s4);
+                updateContent(s5, mergedData.selection.s5);
+                updateContent(s6, mergedData.selection.s6);
+                updateContent(s7, mergedData.selection.s7);
+                updateContent(s8, mergedData.selection.s8);
+                updateContent(s9, mergedData.selection.s9);
+                updateContent(s10, mergedData.slection.s10);
+                updateContent(s11, mergedData.slection.s11);
+                updateContent(s12, mergedData.slection.s12);
+                updateContent(s13, mergedData.slection.s13);
+                updateContent(s14, mergedData.slection.s14);
+                updateContent(s15, mergedData.slection.s15);
+                updateContent(s16, mergedData.slection.s16);
+                updateContent(s17, mergedData.slection.s17);
+                updateContent(s18, mergedData.slection.s18);
+                updateContent(s19, mergedData.slection.s19);
+                updateContent(s20, mergedData.slection.s20);
+                updateContent(s21, mergedData.slection.s21);
+                updateContent(s22, mergedData.slection.s22);
+                updateContent(s23, mergedData.slection.s23);
+                updateContent(s24, mergedData.slection.s24);
+                updateContent(s25, mergedData.slection.s25);
+                updateContent(s26, mergedData.slection.s26);
+                updateContent(s27, mergedData.slection.s27);
+                updateContent(s28, mergedData.slection.s28);
+                updateContent(s29, mergedData.slection.s29);
+                updateContent(s30, mergedData.slection.s30);
+
+                updateContent(se0, mergedData.service.se0);
+                updateContent(se1, mergedData.service.se1);
+                updateContent(se2, mergedData.service.se2);
+                updateContent(se3, mergedData.service.se3);
+                updateContent(se4, mergedData.service.se4);
+                updateContent(se5, mergedData.service.se5);
+
+                updateContent(n0, mergedData.newcars.n0);
+                updateContent(n1, mergedData.newcars.n1);
+                updateContent(n2, mergedData.newcars.n2);
+                updateContent(n3, mergedData.newcars.n3);
+                updateContent(n4, mergedData.newcars.n4);
+                updateContent(n5, mergedData.newcars.n5);
+                updateContent(n6, mergedData.newcars.n6);
+                updateContent(n7, mergedData.newcars.n7);
+                updateContent(n8, mergedData.newcars.n8);
+                updateContent(n9, mergedData.newcars.n9);
+                updateContent(n10, mergedData.newcars.n10);
+                updateContent(n11, mergedData.newcars.n11);
+                updateContent(n12, mergedData.newcars.n12);
+                updateContent(n13, mergedData.newcars.n13);
+
+                updateContent(fc0, mergedData.featuredcars.fc0);
+                updateContent(fc1, mergedData.featuredcars.fc1);
+                updateContent(fc2, mergedData.featuredcars.fc2);
+                updateContent(fc3, mergedData.featuredcars.fc3);
+                updateContent(fc4, mergedData.featuredcars.fc4);
+                updateContent(fc5, mergedData.featuredcars.fc5);
+                updateContent(fc6, mergedData.featuredcars.fc6);
+                updateContent(fc7, mergedData.featuredcars.fc7);
+                updateContent(fc8, mergedData.featuredcars.fc8);
+                updateContent(fc9, mergedData.featuredcars.fc9);
+                updateContent(fc10, mergedData.featuredcars.fc10);
+                updateContent(fc11, mergedData.featuredcars.fc11);
+                updateContent(fc12, mergedData.featuredcars.fc12);
+                updateContent(fc13, mergedData.featuredcars.fc13);
+                updateContent(fc14, mergedData.featuredcars.fc14);
+                updateContent(fc15, mergedData.featuredcars.fc15);
+                updateContent(fc16, mergedData.featuredcars.fc16);
+                updateContent(fc17, mergedData.featuredcars.fc17);
+                updateContent(fc18, mergedData.featuredcars.fc18);
+                updateContent(fc19, mergedData.featuredcars.fc19);
+                updateContent(fc20, mergedData.featuredcars.fc20);
+                updateContent(fc21, mergedData.featuredcars.fc21);
+                updateContent(fc22, mergedData.featuredcars.fc22);
+                updateContent(fc23, mergedData.featuredcars.fc23);
+                updateContent(fc24, mergedData.featuredcars.fc24);
+                updateContent(fc25, mergedData.featuredcars.fc25);
+                updateContent(fc26, mergedData.featuredcars.fc26);
+                updateContent(fc27, mergedData.featuredcars.fc27);
+                updateContent(fc28, mergedData.featuredcars.fc28);
+                updateContent(fc29, mergedData.featuredcars.fc29);
+                updateContent(fc30, mergedData.featuredcars.fc30);
+                updateContent(fc31, mergedData.featuredcars.fc31);
+                updateContent(fc32, mergedData.featuredcars.fc32);
+                updateContent(fc33, mergedData.featuredcars.fc33);
+                updateContent(fc34, mergedData.featuredcars.fc34);
+                updateContent(fc35, mergedData.featuredcars.fc35);
+                updateContent(fc36, mergedData.featuredcars.fc36);
+                updateContent(fc37, mergedData.featuredcars.fc37);
+                updateContent(fc38, mergedData.featuredcars.fc38);
+                updateContent(fc39, mergedData.featuredcars.fc39);
+                updateContent(fc40, mergedData.featuredcars.fc40);
+                updateContent(fc41, mergedData.featuredcars.fc41);
+                updateContent(fc42, mergedData.featuredcars.fc42);
+                updateContent(fc43, mergedData.featuredcars.fc43);
+                updateContent(fc44, mergedData.featuredcars.fc44);
+                updateContent(fc45, mergedData.featuredcars.fc45);
+                updateContent(fc46, mergedData.featuredcars.fc46);
+                updateContent(fc47, mergedData.featuredcars.fc47);
+                updateContent(fc48, mergedData.featuredcars.fc48);
+                updateContent(fc49, mergedData.featuredcars.fc49);
+                updateContent(fc50, mergedData.featuredcars.fc50);
+
+       
+
+                updateContent(c0, mergedData.slide.c0);
+                updateContent(c1, mergedData.slide.c1);
+                updateContent(c2, mergedData.slide.c2);
+                updateContent(c3, mergedData.slide.c3);
+                updateContent(c4, mergedData.slide.c4);
+                updateContent(c5, mergedData.slide.c5);
+                updateContent(c6, mergedData.slide.c6);
+                updateContent(c7, mergedData.slide.c7);
+                updateContent(c8, mergedData.slide.c8);
+                updateContent(c9, mergedData.slide.c9);
+
+                updateContent(f0, mergedData.footer.f0);
+                updateContent(f1, mergedData.footer.f1);
+                updateContent(f2, mergedData.footer.f2);
+                updateContent(f3, mergedData.footer.f3);
+                updateContent(f4, mergedData.footer.f4);
+                updateContent(f5, mergedData.footer.f5);
+                updateContent(f6, mergedData.footer.f6);
+                updateContent(f7, mergedData.footer.f7);
+                updateContent(f8, mergedData.footer.f8);
+                updateContent(f9, mergedData.footer.f9);
+                
+                
+    
+                updateBackgroundColor(g0, mergedData.color.nav);
+                updateBackgroundColor(g1, mergedData.color.header);
+                updateBackgroundColor(g2, mergedData.color.selection);
+                updateBackgroundColor(g3, mergedData.color.service);
+                updateBackgroundColor(g4, mergedData.color.newcars);
+                updateBackgroundColor(g5, mergedData.color.featuredcars);
+                updateBackgroundColor(g6, mergedData.color.clientsays);
+                updateBackgroundColor(g7, mergedData.color.brand);
+                updateBackgroundColor(g8, mergedData.color.blog);
+                updateBackgroundColor(g9, mergedData.color.contact);
+                updateBackgroundColor(g10, mergedData.color.footer);
+             
+                updateImageSrc(img1, mergedData.images.img1);
+                updateImageSrc(img2, mergedData.images.img2);
+                updateImageSrc(img3, mergedData.images.img3);
+                updateImageSrc(img4, mergedData.images.img4);
+                updateImageSrc(img5, mergedData.images.img5);
+                updateImageSrc(img6, mergedData.images.img6);
+                updateImageSrc(img7, mergedData.images.img7);
+                updateImageSrc(img8, mergedData.images.img8);
+                updateImageSrc(img9, mergedData.images.img9);
+                updateImageSrc(img10, mergedData.images.img10);
+                updateImageSrc(img11, mergedData.images.img11);
+                updateImageSrc(img12, mergedData.images.img12);
+                updateImageSrc(img13, mergedData.images.img13);
+                updateImageSrc(img14, mergedData.images.img14);
+                updateImageSrc(img15, mergedData.images.img15);
+                updateImageSrc(img16, mergedData.images.img16);
+                updateImageSrc(img17, mergedData.images.img17);
+                updateImageSrc(img18, mergedData.images.img18);   
+                updateImageSrc(img19, mergedData.images.img19);
+                updateImageSrc(img20, mergedData.images.img20);
+               
+      
+            }
+        })
+        .catch(error => console.error('Error fetching or parsing JSON:', error));
+}
+
+// Call the function to update content when the page loads
+window.addEventListener('load', function () {
+    console.log('page loaded');
+    // Get the current page URL
+    const currentPage = window.location.pathname;
+
+    // Update content based on the current page
+    updatePageContent(currentPage);
+});
